86bac3775449956210a2a51d2c622aef08f52bc6
YassG4mer <yassinekrika2016@gmail.com>
2023-04-29 12:22:08 +0100

    shared key

---
+++ test.js
@@ -0,0 +1,86 @@
+import crypto from 'crypto'
+
+class Ecc {
+
+    // Generate a random private key
+    generatePrivateKey() {
+        // Create a random 256-bit integer
+        const privateKey = crypto.randomBytes(32);
+        return privateKey;
+    }
+
+    // Generate a public key from a private key
+    generatePublicKey(privateKey) {
+        // Calculate the public key from the private key
+        const x = privateKey[0] + privateKey[1] * 256 + privateKey[2] * 65536 + privateKey[3] * 16777216;
+        const y = privateKey[4] + privateKey[5] * 256 + privateKey[6] * 65536 + privateKey[7] * 16777216;
+        const point = {
+            x: x,
+            y: y,
+        };
+        return point;
+    }
+
+    // Calculate the shared secret between two public keys
+    calculateSharedSecret(publicKey1, publicKey2) {
+        // Calculate the shared secret from the two public keys
+        const x = publicKey1.x * publicKey2.y - publicKey1.y * publicKey2.x;
+        const y = publicKey1.x * publicKey2.x + publicKey1.y * publicKey2.y;
+        const n = 115792089237316195423570985008687907852837564279074904382605163141518161494336;
+        const z = 1;
+        while (z > 1) {
+            const t = x / z;
+            x = z;
+            z = t;
+        }
+        return x % n;
+    }
+
+    // Encrypt a message using a public key
+    encryptMessage(message, publicKey) {
+        // Convert the message to a byte array
+        const bytes = Buffer.from(message);
+        // Calculate the shared secret between the public key and the private key
+        const sharedSecret = this.calculateSharedSecret(publicKey, privateKey);
+        // Encrypt each byte of the message using the shared secret
+        for (let i = 0; i < bytes.length; i++) {
+            bytes[i] = bytes[i] ^ sharedSecret;
+        }
+        // Return the encrypted message
+        return bytes;
+    }
+
+    // Decrypt a message using a private key
+    decryptMessage(message, privateKey) {
+        // Convert the message to a byte array
+        const bytes = Buffer.from(message);
+        // Calculate the shared secret between the public key and the private key
+        const sharedSecret = this.calculateSharedSecret(publicKey, privateKey);
+        // Decrypt each byte of the message using the shared secret
+        for (let i = 0; i < bytes.length; i++) {
+            bytes[i] = bytes[i] ^ sharedSecret;
+        }
+        // Return the decrypted message
+        return bytes.toString();
+    }
+
+}
+
+const ecc = new Ecc()
+const message = 'this is a message'
+
+// Generate a public and private key pair
+const privateKey = ecc.generatePrivateKey();
+const publicKey = ecc.generatePublicKey(privateKey);
+
+// Encrypt the fingerprint bitstream
+const encryptedFingerprint = ecc.encryptMessage(message, publicKey);
+
+// Decrypt the encrypted fingerprint
+const decryptedFingerprint = ecc.decryptMessage(encryptedFingerprint, privateKey);
+
+console.log(message)
+
+console.log(privateKey)
+
+console.log(decryptedFingerprint)
